9365b619726c73603997a68d8fa067e064517d50
hmzza <ab1502144@gmail.com>
2024-03-07 11:21:34 +0500

    trying hatespeech rrealtime audio detection

--- lib/threatDetection_mainPage.dart
+++ lib/threatDetection_mainPage.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'dart:io';
-import 'dart:async';
+import 'package:safeguardher/utils/custom_app_bar.dart';
+import 'dart:convert';
 
 void main() => runApp(MyApp());
 
@@ -25,36 +27,25 @@ class AudioRecorderUploader extends StatefulWidget {
 class _AudioRecorderUploaderState extends State<AudioRecorderUploader> {
   FlutterSoundRecorder _audioRecorder = FlutterSoundRecorder();
   bool _isRecorderInitialized = false;
-  bool _isRecording = false;
-  String _response = 'Press the button to record and upload audio.';
-  String? filePath;
+  String _response = 'Press the button to start continuous recording.';
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
     _requestPermissionsAndInit();
-    getFileName();
   }
 
   Future<void> _requestPermissionsAndInit() async {
-    print("_requestPermissionsAndInit data");
-    var micStatus = await Permission.microphone.request();
-    var storageStatus = await Permission.storage.request();
-    print(micStatus);
-    print(storageStatus);
+    final micStatus = await Permission.microphone.request();
+    final storageStatus = await Permission.storage.request();
     if (micStatus != PermissionStatus.granted || storageStatus != PermissionStatus.granted) {
       setState(() {
         _response = 'Microphone or Storage permission not granted';
       });
       return;
     }
-
     await _initRecorder();
-  }
-
-  Future<void> getFileName() async {
-    Directory tempDir = await getTemporaryDirectory();
-    filePath = '${tempDir.path}/temp.wav';
   }
 
   Future<void> _initRecorder() async {
@@ -71,56 +62,50 @@ class _AudioRecorderUploaderState extends State<AudioRecorderUploader> {
     }
   }
 
-  Future<void> _startRecording() async {
+  void _startContinuousRecording() async {
+    if (!_isRecorderInitialized) return;
+    setState(() {
+      _response = 'Recording started...';
+    });
 
-    try {
+    const chunkDuration = Duration(seconds: 5);
+    _timer = Timer.periodic(chunkDuration, (timer) async {
+      final tempDir = await getTemporaryDirectory();
+      final filePath = '${tempDir.path}/${DateTime.now().millisecondsSinceEpoch}.wav';
       await _audioRecorder.startRecorder(toFile: filePath);
-      setState(() {
-        _isRecording = true;
-        _response = 'Recording...';
-      });
-    } catch (e) {
-      setState(() {
-        _response = 'Failed to start recording: $e';
-      });
-    }
+      await Future.delayed(chunkDuration); // Wait for 5 seconds
+      await _audioRecorder.stopRecorder();
+      await _uploadFile(filePath);
+    });
   }
 
-  Future<void> _stopRecordingAndUpload() async {
-    print("_stopRecordingAndUpload:");
-    print(filePath);
-    try {
-      await _audioRecorder.stopRecorder();
-      setState(() {
-        _isRecording = false;
-      });
-      if (filePath != null) {
-        _response = 'Uploading...';
-        await _uploadFile(filePath!);
-      } else {
-        setState(() {
-          _response = 'Recording failed, file path is null';
-        });
-      }
-    } catch (e) {
-      setState(() {
-        _response = 'Failed to stop recording: $e';
-      });
-    }
+  Future<void> _stopRecording() async {
+    _timer.cancel();
+    await _audioRecorder.stopRecorder();
+    setState(() {
+      _response = 'Recording stopped.';
+    });
   }
 
   Future<void> _uploadFile(String filePath) async {
     try {
-      var request = http.MultipartRequest('POST', Uri.parse('https://118a-115-186-57-250.ngrok-free.app/process')); // Update with your endpoint
+      var request = http.MultipartRequest('POST', Uri.parse('https://cc6f-58-65-135-186.ngrok-free.app/process'));
       request.files.add(await http.MultipartFile.fromPath('file', filePath));
       var response = await request.send();
 
       final respStr = await response.stream.bytesToString();
-
       if (response.statusCode == 200) {
-        setState(() {
-          _response = 'Upload successful: $respStr';
-        });
+        // Assuming the response body contains a JSON object with a boolean field "hateSpeech"
+        final decodedResp = jsonDecode(respStr); // Decode the JSON response
+        final bool hateSpeechDetected = decodedResp['hateSpeech'] ?? false; // Check for hate speech detection
+
+        if (hateSpeechDetected) {
+          _timer.cancel();
+          await _audioRecorder.stopRecorder(); // Ensure recording is stopped
+          setState(() {
+            _response = 'Hate speech detected. Recording stopped.';
+          });
+        }
       } else {
         setState(() {
           _response = 'Failed to upload audio. Response code: ${response.statusCode}';
@@ -133,31 +118,49 @@ class _AudioRecorderUploaderState extends State<AudioRecorderUploader> {
     }
   }
 
+
   @override
   void dispose() {
     _audioRecorder.closeRecorder();
+    _timer.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Audio Recorder & Uploader')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _isRecorderInitialized && !_isRecording ? _startRecording : null,
-              child: Text('Start Recording'),
-            ),
-            ElevatedButton(
-              onPressed: _isRecording ? _stopRecordingAndUpload : null,
-              child: Text('Stop and Upload'),
-            ),
-            SizedBox(height: 20),
-            Text(_response),
-          ],
+      appBar: CustomAppBar(titleText: 'Threat Detector'),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/backgroundlogin.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _isRecorderInitialized ? _startContinuousRecording : null,
+                child: Text('Start Continuous Recording'),
+              ),
+              ElevatedButton(
+                onPressed: () => _stopRecording(),
+                child: Text(
+                  'Stop Recording',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  side: BorderSide(color: Colors.white, width: 2),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(_response, style: TextStyle(color: Colors.white)),
+            ],
+          ),
         ),
       ),
     );
